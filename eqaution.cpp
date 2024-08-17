f429623f2ff2a6e71fa3d2208739cd53c0bb4d9d
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-08-18 00:07:26 +0530

    Create eqaution.cpp

---
+++ eqaution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() { 
+    int n;
+    cin>>n;
+
+    if(n==1){
+      int a=9,b=8;
+      cout<<a<<" "<<b;
+      return 0;
+    }else{
+      int a=3*n,b=2*n;
+      cout<<a<<" "<<b;
+    }
+  
+  
+}
